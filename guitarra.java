31156d563061838dd744ceebd4a2e2558064b945
Joaquim Júnior <juniorpimenta@gmail.com>
2023-05-02 20:32:05 -0300

    Create guitarra.java

---
+++ guitarra.java
@@ -0,0 +1,59 @@
+Guitarra.java
+package unidade_1;
+//declaração da classe
+public class Guitarra {
+private String numeroSerie, fabricante, modelo, tipo, madeira; private double preso
+//Criação do construtor da classe Guitarra
+public Guitarra(
+String numeroseris, String fabricante,
+String modelo,
+String madeira,
+String tipo,
+double prese) {
+this.numeroSerie - numeroSerie;
+this fabricante
+fabricante;
+this.modelo modelo;
+this.ting tipo;
+}
+this.madsica- madeira;
+this.preco preco;
+public String getNumeroSerie() {
+}
+return numeroSerie;
+public void setNumeroseris(String numerosscis) {
+this.numeroSerie = numeroSerie;
+}
+public String getFabricante() {
+}
+return fabricante;
+public void setFabricante(String fabricante) {
+}
+this.fabricante = fabricante;
+public String getModelo() {
+}
+return modelo;
+public void settledelo(String modelo) {
+}
+this.modelo modelo;
+public String gettiro() {
+}
+return tipo;
+public void setting(String tipo) {
+}
+this.ting - tipo;
+public String getMadeira() {
+}
+catuco madeira;
+puklis void setűladeira(String madeira) {
+}
+this.madsica - madeira;
+public double getireco() {
+}
+catuco Rosse;
+public void setereco(double preco) {
+}
+this.press = prese;
+//o método main() é o usteda principal da classe Ruklis static void main(String[] args) {
+//instanciamos um objeto chamado "sinhaGuitara" que será do tipo "Guitarra" //Os valores passados são usados pelo construtor da classe para criar //o objeto
+Guitarra minhaGuitarra = new Guitarra("81828384", "fender", "telecaster", "elétrica", "mogno", 1500);
